6b0315fc1816e45e9c002fbb914790fd6b2fa5d5
paulsikmin <minmediaservice@gmail.com>
2023-08-02 09:19:04 +0900

    [ADD&FIX] 23/07/31~23/08/01 공지사항 관리 기능 추가 및 회원관리 수정
    1. 공지사항 등록 기능 추가
    2. 공지사항 전체 조회 기능 추가
    3. 공지사항 상세 조회 기능 추가
    4. 회원 관련, 성공/실패 jsp파일 /WEB-INF/views/ 아래로 이동, 그에 따른 컨트롤러 코드
    수정(RequestDispatcher로 페이지 이동)

--- src/main/java/member/controller/EnrollController.java
+++ src/main/java/member/controller/EnrollController.java
@@ -30,7 +30,7 @@ public class EnrollController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		request.getRequestDispatcher("/WEB-INF/views/member/enroll.jsp").forward(request, response);
 	}
 
 	/**
