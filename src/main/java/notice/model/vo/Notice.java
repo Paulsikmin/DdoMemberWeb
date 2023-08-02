c05ca3ef391efaea247d08cbef688e9a409d4fa4
paulsikmin <minmediaservice@gmail.com>
2023-08-02 16:47:28 +0900

    [ADD] 23/08/02 공지사항 수정 및 페이징 처리 추가
    1. 공지사항 상세 페이지에서 수정하기를 눌러 수정페이지로 이동 후 내용 수정 후 공지사항 수정하는 기능 추가 완료
    2. 페이지 처리 기능 추가 리스트 페이지에 페이지 네비게이션 추가 완료
    3. DAO에서 전체 게시물의 갯수 구하는 메소드는 미완성, 하드코딩으로 고정되어 있음.

--- src/main/java/notice/model/vo/Notice.java
+++ src/main/java/notice/model/vo/Notice.java
@@ -19,6 +19,13 @@ public class Notice {
 		this.noticeContent = noticeContent;
 	}
 
+	public Notice(int noticeNo, String noticeSubject, String noticeContent) {
+		super();
+		this.noticeNo = noticeNo;
+		this.noticeSubject = noticeSubject;
+		this.noticeContent = noticeContent;
+	}
+
 	public int getNoticeNo() {
 		return noticeNo;
 	}
